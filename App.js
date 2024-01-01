d5f1f60ae56130c43a7c1a43d9358bb410aa335e
goyalsunil452 <goyalsunil452@gmail.com>
2024-01-01 23:37:55 +0530

    part-1_React_NPM_Parcel

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
+import React from 'react';   
+/*
+*Plain javascript does not understand the 
+import that why we need provide type = "module" 
+in index.html
+*/
+// import ReactDOM from 'react-dom';  this wont work as react changes the path
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement("div",{id:"parent", class:"parent-class"},
   [React.createElement("div",{id:'child1'},"inner div 1"),
-  React.createElement("div",{id:'child1'},"inner div 2")]
+  React.createElement("div",{id:'child1'},"inner div 2"),
+  React.createElement("div",{id:'child1'},"inner div 3")]
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
---
+++ README.md
@@ -0,0 +1,27 @@
+# React Js with help of npm
+
+# What does Parcel do? (https://parceljs.org/)
+ - Dev Build
+ - Local Server
+ - HMR = HOT Module Replacement (Automatically refereshes the page)
+ - File watching Algorithm - written in C++
+ - Caching - Faster Builds
+ - Image Optimization 
+ - Minification
+ - Bundling
+ - Compressing
+ - Consistant Hashing
+ - Code Splitting
+ - Differential Bundling - Support Older/Different browser
+ - Diagnostic
+ - Error Handling
+ - Provide HTTPs Support
+ - Tree Shanking - Remove unused code
+ - Provide Workers
+ - Lazy mode
+ - Different dev and prod bundles 
+
+
+# browserslist (https://browserslist.dev/)
+- Provides the supports for browsers 
+
